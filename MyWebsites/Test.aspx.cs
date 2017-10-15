8754fafeb9ab2d48586bf9adacb771e70ca08b7b
abinasha123 <abi.hota180@gmail.com>
2017-10-15 21:49:16 +0530

    my first commit

---
+++ MyWebsites/Test.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+namespace MyWebsites
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "Please click the button";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "Button is clicked1";
+           System.Diagnostics.Trace.TraceError("Working");
+            System.Diagnostics.Trace.TraceError("Slow response - database01");
+
+
+
+        }
+    }
+}
---
+++ MyWebsites/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyWebsites.Global" Language="C#" %>
